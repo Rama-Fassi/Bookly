d3854a1f87e363f4d6d2a89fab7ee4f8129888b1
rama-fassi <rama.fassi@gmail.com>
2024-10-19 17:13:36 +0300

    add domain_entities

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final num rating;
+  final num price;
+  final String authorName;
+
+  BookEntity(this.image, this.title, this.rating, this.price, this.authorName);
+
+}
